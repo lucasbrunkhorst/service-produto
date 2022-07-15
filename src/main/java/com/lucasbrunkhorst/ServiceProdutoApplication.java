7bcaf6d004cb4bdbb452c9c71f60dad843b27b45
lucasbrunkhorst <lukasb08@hotmail.com>
2022-07-15 18:54:42 -0300

    init

---
+++ src/main/java/com/lucasbrunkhorst/ServiceProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.lucasbrunkhorst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProdutoApplication.class, args);
+	}
+
+}
